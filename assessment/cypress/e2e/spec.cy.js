d36f680c7be875bbf43e655a81b9754f96f3a2e0
CrioUser <crio-user@criodo.com>
2024-05-28 10:09:52 +0530

    ME_FE_BUILDOUT_XPROFILE_MODULE_ONE

--- assessment/cypress/e2e/spec.cy.js
+++ assessment/cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('XProfile Tests', () => {
   beforeEach(() => {
     // Visits the page before each test
-    cy.visit('http://localhost:3000/'); // Replace with your actual URL
+    cy.visit('http://15.207.101.173:8081/workspace/amaiyaaditya-ME_FE_BUILDOUT_XPROFILE/') // Replace with your actual URL
   });
 
   it('has the correct title', () => {
